23f2493f2105e4ea997dc40e4e2b2954cd9f04fc
oussema makni <oussema.makni@esprit.tn>
2019-12-08 19:42:55 +0100

    ched

---
+++ view/recovery/rec.php
@@ -0,0 +1,42 @@
+<html> 
+    
+<head>
+
+</head>
+
+<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
+
+<body style="background:transparent;">
+    
+</body>
+</html>
+
+<?php
+
+include "../../core/userC.php";
+
+$user1C = new userC();
+
+    if (!empty($_POST['email'])) {
+        if ($user1C->emailExists($_POST['email'])) {
+            echo ('<script>swal("OUPS !", "mmmm !", "error", {
+                buttons: false,
+                timer: 1729
+            }); </script>');
+        } else {
+            echo ('<script>swal("OUPS !", "Email doesn t exist !", "error", {
+                buttons: false,
+                timer: 1729
+            }); </script>');
+        }
+    } else {
+        echo ('<script>swal("OUPS !", "Insert an Email please !", "error", {
+            buttons: false,
+            timer: 1729
+        }); </script>');
+    }
+
+
+
+?>
---
+++ view/recovery/recuperer.php
@@ -0,0 +1,57 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Login</title>
+    <script src="../assets/js/vendor/jquery-1.11.2.min.js"></script>
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="../assets/js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="http://ajax.googleapis.com/ajax/libs/jquery/1.7.2/jquery.min.js"></script>
+    <link href="https://fonts.googleapis.com/css?family=Raleway:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
+
+    <link rel="stylesheet" href="../login.css">
+    <script src="../Login.js"></script>
+
+</head>
+
+<body style="background: #FFCC33;">
+
+    <div class="container">
+        <div class="row">
+            <br>
+            <a href="../index.html"><img src="../assets/images/logo.png" height="50" width="150"></a>
+            <h2>Recover your password !</h2>
+
+        </div>
+    </div>
+    <br>
+    <br>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-4 col-md-offset-4">
+                <div class="form-body">
+                    <div class="tab-content">
+                        <div id="sectionA" class="tab-pane fade in active">
+                            <div class="innter-form">
+                                <form class="sa-innate-form" method="POST" action="rec.php">
+                                    <label style="color: aliceblue;">Email Address</label>
+                                    <input type="text" name="email" placeholder="Email address">
+                                    <button type="button">Recover</button>
+                                </form>
+                            </div>
+                            <div class="clearfix"></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</body>
+
+
+</html>
+
