ae6157495937b509ffd038d333d6a5aa812fb2d9
oussema makni <oussema.makni@esprit.tn>
2019-12-09 16:21:06 +0100

    send email prob

--- core/userC.php
+++ core/userC.php
@@ -6,72 +6,68 @@ class userC
 {
 
     function findEmail($user)
-    { 
-        $email=$user->getEmail();
+    {
+        $email = $user->getEmail();
         $sql = "SELECT * FROM users where email=$email";
-        $db= config::getConnection();
-        try{
-            $liste=$db->query($sql);
-            }
-        catch (Exception $e){
-            die('Erreur: '.$e->getMessage());
-            }
+        $db = config::getConnection();
+        try {
+            $liste = $db->query($sql);
+        } catch (Exception $e) {
+            die('Erreur: ' . $e->getMessage());
+        }
     }
     function findRole($email)
     {
         $sql = "SELECT Account_Type FROM users where email='$email'";
-        $db= config::getConnection();
-        try{
-            $role=$db->query($sql);
-            }
-        catch (Exception $e){
-            die('Erreur: '.$e->getMessage());
-            }
-            return $role->fetchColumn();
+        $db = config::getConnection();
+        try {
+            $role = $db->query($sql);
+        } catch (Exception $e) {
+            die('Erreur: ' . $e->getMessage());
+        }
+        return $role->fetchColumn();
     }
     function findName($email)
     {
         $sql = "SELECT Full_name FROM users where email='$email'";
-        $db= config::getConnection();
-        try{
-            $name=$db->query($sql);
-            }
-        catch (Exception $e){
-            die('Erreur: '.$e->getMessage());
-            }
-            return $name->fetchColumn();
+        $db = config::getConnection();
+        try {
+            $name = $db->query($sql);
+        } catch (Exception $e) {
+            die('Erreur: ' . $e->getMessage());
+        }
+        return $name->fetchColumn();
     }
     function findID($email)
     {
         $sql = "SELECT Account_ID FROM users where email='$email'";
-        $db= config::getConnection();
-        try{
-            $name=$db->query($sql);
-            }
-        catch (Exception $e){
-            die('Erreur: '.$e->getMessage());
-            }
-            return $name->fetchColumn();
+        $db = config::getConnection();
+        try {
+            $name = $db->query($sql);
+        } catch (Exception $e) {
+            die('Erreur: ' . $e->getMessage());
+        }
+        return $name->fetchColumn();
     }
     function findPassword($email)
-    { 
+    {
 
         $sql = "SELECT password FROM users where email='$email'";
-        $db= config::getConnection();
-        try{
-            $enc_password=$db->query($sql);
-            }
-        catch (Exception $e){
-            die('Erreur: '.$e->getMessage());
-            }
-            return $enc_password->fetchColumn();
+        $db = config::getConnection();
+        try {
+            $enc_password = $db->query($sql);
+        } catch (Exception $e) {
+            die('Erreur: ' . $e->getMessage());
+        }
+        return $enc_password->fetchColumn();
     }
 
 
-    function emailExists($email) {
-        $pdo= config::getConnection();
-        $stmt = $pdo->prepare("SELECT 1 FROM users WHERE email=?");
-        $stmt->execute([$email]); 
+    function emailExists($email)
+    {
+        $pdo = config::getConnection();
+        $stmt = $pdo->prepare("SELECT email FROM users WHERE email=?");
+        $stmt->execute([$email]);
         return $stmt->fetchColumn();
     }
 
@@ -79,36 +75,81 @@ class userC
 
     function addUser($user)
     {
-        
- 
 
         $sql = "INSERT into users (Full_name,email,password) 
                 values (:name,:email,:password) ";
 
         $db = config::getConnection();
 
-        try{
+        try {
 
-        $req=$db->prepare($sql);
-		
-        $name=$user->getName();
-        $email=$user->getEmail();
-        $password=$user->getPassword();
-        $enc_password=password_hash($password,PASSWORD_DEFAULT);
-    
-        $req->bindValue(':name',$name);
-        $req->bindValue(':email',$email);
-        $req->bindValue(':password',$enc_password);
+            $req = $db->prepare($sql);
 
-        
-        $req->execute();
-    
+            $name = $user->getName();
+            $email = $user->getEmail();
+            $password = $user->getPassword();
+            $enc_password = password_hash($password, PASSWORD_DEFAULT);
 
-        }catch(Exception $e){
-            die('Erreur: '. $e->getMessage());
+            $req->bindValue(':name', $name);
+            $req->bindValue(':email', $email);
+            $req->bindValue(':password', $enc_password);
+
+
+            $req->execute();
+        } catch (Exception $e) {
+            die('Erreur: ' . $e->getMessage());
+        }
+    }
+}
+
+class recoverC
+{
+    function emailExists($email)
+    {
+        $pdo = config::getConnection();
+        $stmt = $pdo->prepare("SELECT email FROM recovery WHERE email=?");
+        $stmt->execute([$email]);
+        return $stmt->fetchColumn();
+    }
+
+    function update($email, $code)
+    {
+        $sql = "UPDATE recovery SET code=:code WHERE email='$email'";
+
+        $db = config::getConnection();
+        try {
+
+            $req = $db->prepare($sql);
+
+            $req->bindValue(':code', $code);
+
+            $req->execute();
+
+            $s = $req->execute();
+        } catch (Exception $e) {
+            echo " Erreur ! " . $e->getMessage();
         }
     }
 
+    function addrecover($code, $email)
+    {
 
 
+        $sql = "INSERT into recovery (code,email) 
+                values (:code,:email) ";
+
+        $db = config::getConnection();
+
+        try {
+
+            $req = $db->prepare($sql);
+
+            $req->bindValue(':code', $code);
+            $req->bindValue(':email', $email);
+
+            $req->execute();
+        } catch (Exception $e) {
+            die('Erreur: ' . $e->getMessage());
+        }
+    }
 }
--- view/recovery/rec.php
+++ view/recovery/rec.php
@@ -1,5 +1,5 @@
-<html> 
-    
+<html>
+
 <head>
 
 </head>
@@ -7,35 +7,94 @@
 <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 
 
-<body style="background:transparent;">
-    
+<body>
+
 </body>
+
 </html>
 
 <?php
 
+
 include "../../core/userC.php";
 
 $user1C = new userC();
+$recover1C = new recoverC();
+$i = 0;
 
-    if (!empty($_POST['email'])) {
-        if ($user1C->emailExists($_POST['email'])) {
-            echo ('<script>swal("Done!", "An email to recover your password has been sent!", "success", {
-                buttons: false,
-                timer: 1729
-            }); </script>');
-        } else {
-            echo ('<script>swal("OUPS !", "Email doesn t exist !", "error", {
-                buttons: false,
-                timer: 1729
-            }); </script>');
+if (!empty($_POST['email'])) {
+    if ($user1C->emailExists($_POST['email'])) {
+
+        $recup_email = htmlspecialchars($_POST['email']);
+
+        $recup_code = "";
+        for( $i ; $i < 8 ; $i++ ){
+          $recup_code .= mt_rand(0,9);
         }
+
+        $pseudo = $user1C->findName($recup_email);
+
+        if($recover1C->emailExists($recup_email) == $recup_email){
+          $recover1C->update($recup_email,$recup_code);
+        }
+        else{
+          $recover1C->addrecover($recup_code,$recup_email);
+          echo "done";
+        }
+
+        $header = "MIME-Version: 1.0\r\n";
+        $header .= 'From:"[Work UP]"<elbeuff@gmail.com>' . "\n";
+        $header .= 'Content-Type:text/html; charset="utf-8"' . "\n";
+        $header .= 'Content-Transfer-Encoding: 8bit';
+        $message = '
+         <html>
+         <head>
+           <title>Récupération de mot de passe !</title>
+           <meta charset="utf-8" />
+         </head>
+         <body>
+           <font color="#303030";>
+             <div align="center">
+               <table width="600px">
+                 <tr>
+                   <td>
+                     
+                    <div align="center">Bonjour <b>' . $pseudo . '</b>,</div>
+                    cliquez <a href="localhost/work-up_website/view/recovery/rec.php?section=code&code='. $recup_code .'>ici</a> pour reinitialiser votre mot de passe !<br>
+                    A bientot sur <a href="http://localhost/work-up_website/view/index.php">Work UP</a>!!
+                    
+
+                   </td>
+                 </tr>
+                 <tr>
+                   <td align="center">
+                     <font size="2">
+                       Ceci est un email automatique, merci de ne pas y répondre
+                     </font>
+                   </td>
+                 </tr>
+               </table>
+             </div>
+           </font>
+         </body>
+         </html>
+         ';
+         mail($recup_email, "Récupération de mot de passe - Work UP", $message, $header);
+
     } else {
-        echo ('<script>swal("OUPS !", "Insert an Email please !", "error", {
+        echo ('<script>swal("OUPS !", "Email doesn t exist !", "error", {
+                buttons: false,
+                timer: 1729
+            }); </script>');
+        header('location: recuperer.html');
+    }
+} else {
+    echo ('<script>swal("OUPS !", "Insert an Email please !", "error", {
             buttons: false,
             timer: 1729
         }); </script>');
-    }
+    header('location: recuperer.html');
+}
 
 
 
