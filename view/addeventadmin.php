50f5755c7fc80eb58f09d85348e47c5f5d3f95ec
Hassene <mohamedhassene.hamed@esprit.tn>
2019-12-11 18:42:15 +0100

    Another one

--- view/addeventadmin.php
+++ view/addeventadmin.php
@@ -1,4 +1,11 @@
+<?php session_start();
 
+
+
+if($_SESSION['role']!='admin')
+  header('location: loggedin.php');
+
+?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
   <head>
@@ -45,7 +52,6 @@
   <link href="assetsAdmin/img/favicon.png" rel="shortcut icon" />
   <script src="assetsAdmin/plugins/nprogress/nprogress.js"></script>
 </head>
-
 
   <body class="header-fixed sidebar-fixed sidebar-dark header-light" id="body">
     <script>
@@ -186,134 +192,19 @@
     </ul>
   </li>
 
-
-  <li  class="has-sub " >
-    <a class="sidenav-item-link" href="javascript:void(0)" data-toggle="collapse" data-target="#ui-elements"
-      aria-expanded="false" aria-controls="ui-elements">
-      <i class="mdi mdi-folder-multiple-outline"></i>
-      <span class="nav-text">UI Elements</span> <b class="caret"></b>
-    </a>
-    <ul  class="collapse"  id="ui-elements"
-      data-parent="#sidebar-menu">
-      <div class="sub-menu">
-        
-        
-        <li  class="has-sub" >
-          <a class="sidenav-item-link" href="javascript:void(0)" data-toggle="collapse" data-target="#components"
-            aria-expanded="false" aria-controls="components">
-            <span class="nav-text">Components</span> <b class="caret"></b>
-          </a>
-          <ul  class="collapse"  id="components">
-            <div class="sub-menu">
-              
-              <li >
-                <a href="alert.html">Alert</a>
-              </li>
-             
-              
-            </div>
-          </ul>
-        </li>
         
 
         
-        <li  class="has-sub" >
-          <a class="sidenav-item-link" href="javascript:void(0)" data-toggle="collapse" data-target="#icons"
-            aria-expanded="false" aria-controls="icons">
-            <span class="nav-text">Icons</span> <b class="caret"></b>
-          </a>
-          <ul  class="collapse"  id="icons">
-            <div class="sub-menu">
-              
-              <li >
-                <a href="material-icon.html">Material Icon</a>
-              </li>
-              
-              <li >
-                <a href="flag-icon.html">Flag Icon</a>
-              </li>
-              
-            </div>
-          </ul>
-        </li>
+  
+
+        
+        
+
         
 
         
         
-        <li  class="has-sub" >
-          <a class="sidenav-item-link" href="javascript:void(0)" data-toggle="collapse" data-target="#forms"
-            aria-expanded="false" aria-controls="forms">
-            <span class="nav-text">Forms</span> <b class="caret"></b>
-          </a>
-          <ul  class="collapse"  id="forms">
-            <div class="sub-menu">
-              
-              <li >
-                <a href="basic-input.html">Basic Input</a>
-              </li>
-              
-              <li >
-                <a href="input-group.html">Input Group</a>
-              </li>
-              
-              <li >
-                <a href="checkbox-radio.html">Checkbox & Radio</a>
-              </li>
-              
-              <li >
-                <a href="form-validation.html">Form Validation</a>
-              </li>
-              
-              <li >
-                <a href="form-advance.html">Form Advance</a>
-              </li>
-              
-            </div>
-          </ul>
-        </li>
-        
 
-        
-        
-        <li  class="has-sub" >
-          <a class="sidenav-item-link" href="javascript:void(0)" data-toggle="collapse" data-target="#maps"
-            aria-expanded="false" aria-controls="maps">
-            <span class="nav-text">Maps</span> <b class="caret"></b>
-          </a>
-          <ul  class="collapse"  id="maps">
-            <div class="sub-menu">
-              
-              <li >
-                <a href="google-map.html">Google Map</a>
-              </li>
-              
-              <li >
-                <a href="vector-map.html">Vector Map</a>
-              </li>
-              
-            </div>
-          </ul>
-        </li>
-                                
-        <li  class="has-sub" >
-          <a class="sidenav-item-link" href="javascript:void(0)" data-toggle="collapse" data-target="#widgets"
-            aria-expanded="false" aria-controls="widgets">
-            <span class="nav-text">Widgets</span> <b class="caret"></b>
-          </a>
-          <ul  class="collapse"  id="widgets">
-            <div class="sub-menu">
-              
-              <li >
-                <a href="general-widget.html">General Widget</a>
-              </li>
-              
-              <li >
-                <a href="chart-widget.html">Chart Widget</a>
-              </li>
-              
-            </div>
-          </ul>
-        </li>
         
 
         
@@ -325,261 +216,38 @@
 
 
 
-  <li  class="has-sub" >
-    <a class="sidenav-item-link" href="javascript:void(0)" data-toggle="collapse" data-target="#charts"
-      aria-expanded="false" aria-controls="charts">
-      <i class="mdi mdi-chart-pie"></i>
-      <span class="nav-text">Charts</span> <b class="caret"></b>
-    </a>
-    <ul  class="collapse"  id="charts"
-      data-parent="#sidebar-menu">
-      <div class="sub-menu">
-        
-        
-          
-            <li >
-              <a class="sidenav-item-link" href="chartjs.html">
-                <span class="nav-text">ChartJS</span>
-                
-              </a>
-            </li>
-          
-        
-
-        
-      </div>
-    </ul>
-  </li>
 
 
 
 
 
-  <li  class="has-sub" >
-    <a class="sidenav-item-link" href="javascript:void(0)" data-toggle="collapse" data-target="#pages"
-      aria-expanded="false" aria-controls="pages">
-      <i class="mdi mdi-image-filter-none"></i>
-      <span class="nav-text">Pages</span> <b class="caret"></b>
-    </a>
-    <ul  class="collapse"  id="pages"
-      data-parent="#sidebar-menu">
-      <div class="sub-menu">
-        
-        
-          
-            <li >
-              <a class="sidenav-item-link" href="user-profile.html">
-                <span class="nav-text">User Profile</span>
-                
-              </a>
-            </li>
+
           
         
 
         
         
-        <li  class="has-sub" >
-          <a class="sidenav-item-link" href="javascript:void(0)" data-toggle="collapse" data-target="#authentication"
-            aria-expanded="false" aria-controls="authentication">
-            <span class="nav-text">Authentication</span> <b class="caret"></b>
-          </a>
-          <ul  class="collapse"  id="authentication">
-            <div class="sub-menu">
-              
-              <li >
-                <a href="sign-in.html">Sign In</a>
-              </li>
-              
-              <li >
-                <a href="sign-up.html">Sign Up</a>
-              </li>
-              
-            </div>
-          </ul>
-        </li>
-        
+       
 
         
         
-        <li  class="has-sub" >
-          <a class="sidenav-item-link" href="javascript:void(0)" data-toggle="collapse" data-target="#others"
-            aria-expanded="false" aria-controls="others">
-            <span class="nav-text">Others</span> <b class="caret"></b>
-          </a>
-          <ul  class="collapse"  id="others">
-            <div class="sub-menu">
-              
-              <li >
-                <a href="invoice.html">invoice</a>
-              </li>
-              
-              <li >
-                <a href="error.html">Error</a>
-              </li>
-              
-            </div>
-          </ul>
-        </li>
-        
-
-        
-      </div>
-    </ul>
-  </li>
 
 
 
 
 
-  <li  class="has-sub" >
-    <a class="sidenav-item-link" href="javascript:void(0)" data-toggle="collapse" data-target="#documentation"
-      aria-expanded="false" aria-controls="documentation">
-      <i class="mdi mdi-book-open-page-variant"></i>
-      <span class="nav-text">Documentation</span> <b class="caret"></b>
-    </a>
-    <ul  class="collapse"  id="documentation"
-      data-parent="#sidebar-menu">
-      <div class="sub-menu">
-        
-        
-          
-            <li class="section-title">
-              Getting Started
-            </li>
+
           
         
 
         
         
-          
-            <li >
-              <a class="sidenav-item-link" href="introduction.html">
-                <span class="nav-text">Introduction</span>
-                
-              </a>
-            </li>
-          
+
         
 
         
         
-          
-            <li >
-              <a class="sidenav-item-link" href="setup.html">
-                <span class="nav-text">Setup</span>
-                
-              </a>
-            </li>
-          
-        
 
-        
-        
-          
-            <li >
-              <a class="sidenav-item-link" href="customization.html">
-                <span class="nav-text">Customization</span>
-                
-              </a>
-            </li>
-          
-        
-
-        
-        
-          
-            <li class="section-title">
-              Layouts
-            </li>
-          
-        
-
-        
-        
-        <li  class="has-sub" >
-          <a class="sidenav-item-link" href="javascript:void(0)" data-toggle="collapse" data-target="#headers"
-            aria-expanded="false" aria-controls="headers">
-            <span class="nav-text">Layout Headers</span> <b class="caret"></b>
-          </a>
-          <ul  class="collapse"  id="headers">
-            <div class="sub-menu">
-              
-              <li >
-                <a href="header-fixed.html">Header Fixed</a>
-              </li>
-              
-              <li >
-                <a href="header-static.html">Header Static</a>
-              </li>
-              
-              <li >
-                <a href="header-light.html">Header Light</a>
-              </li>
-              
-              <li >
-                <a href="header-dark.html">Header Dark</a>
-              </li>
-              
-            </div>
-          </ul>
-        </li>
-        
-
-        
-        
-        <li  class="has-sub" >
-          <a class="sidenav-item-link" href="javascript:void(0)" data-toggle="collapse" data-target="#sidebar-navs"
-            aria-expanded="false" aria-controls="sidebar-navs">
-            <span class="nav-text">layout Sidebars</span> <b class="caret"></b>
-          </a>
-          <ul  class="collapse"  id="sidebar-navs">
-            <div class="sub-menu">
-              
-              <li >
-                <a href="sidebar-open.html">Sidebar Open</a>
-              </li>
-              
-              <li >
-                <a href="sidebar-minimized.html">Sidebar Minimized</a>
-              </li>
-              
-              <li >
-                <a href="sidebar-offcanvas.html">Sidebar Offcanvas</a>
-              </li>
-              
-              <li >
-                <a href="sidebar-with-footer.html">Sidebar With Footer</a>
-              </li>
-              
-              <li >
-                <a href="sidebar-without-footer.html">Sidebar Without Footer</a>
-              </li>
-              
-              <li >
-                <a href="right-sidebar.html">Right Sidebar</a>
-              </li>
-              
-            </div>
-          </ul>
-        </li>
-        
-
-        
-        
-          
-            <li >
-              <a class="sidenav-item-link" href="rtl.html">
-                <span class="nav-text">RTL Direction</span>
-                
-              </a>
-            </li>
-          
-        
-
-        
-      </div>
-    </ul>
-  </li>
 
 
 
@@ -666,14 +334,14 @@
                   <li class="dropdown user-menu">
                     <button href="#" class="dropdown-toggle nav-link" data-toggle="dropdown">
                       <img src="assetsAdmin/img/user/user.png" class="user-image" alt="User Image" />
-                      <span class="d-none d-lg-inline-block">Abdus Salam</span>
+                      <span class="d-none d-lg-inline-block"><?php if(isset($_SESSION['name']))echo $_SESSION['name'];?></span>
                     </button>
                     <ul class="dropdown-menu dropdown-menu-right">
                       <!-- User image -->
                       <li class="dropdown-header">
-                        <img src="assetsAdmin/img/user/user.png" class="img-circle" alt="User Image" />
+                      <img src="assetsAdmin/img/user/user.png" class="img-circle" alt="User Image" />
                         <div class="d-inline-block">
-                          Abdus Salam <small class="pt-1">abdus@gmail.com</small>
+                        <?php if(isset($_SESSION['name']))echo $_SESSION['name'];?><small class="pt-1"><?php if(isset($_SESSION['email']))echo $_SESSION['email'];?></small>
                         </div>
                       </li>
 
@@ -709,7 +377,7 @@
 
         <div class="content-wrapper">
           <div class="content">							<div class="breadcrumb-wrapper">
-								<h1>Table</h1>
+								<h1>Events</h1>
 								
         <nav aria-label="breadcrumb">
           <ol class="breadcrumb p-0">
@@ -719,9 +387,9 @@
               </a>
             </li>
             <li class="breadcrumb-item">
-              components
+              Gerer Evenements
             </li>
-            <li class="breadcrumb-item" aria-current="page">table</li>
+            <li class="breadcrumb-item" aria-current="page">Afficher Evenements</li>
           </ol>
         </nav>
 
